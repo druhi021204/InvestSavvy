9a61954851e71d90013b40553bf479d099c6d02d
siddhip2004 <shparekh25@gmail.com>
2024-01-13 14:10:02 +0530

    few changes

--- practice projects/mern_stack_p/backend/server.js
+++ practice projects/mern_stack_p/backend/server.js
@@ -1,21 +1,28 @@
 require('dotenv').config()
 
 const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 //express app, stored in app constant
 const app = express();
 
+
 //middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
 
+//after using Express router 
+app.get('/api/workouts', workoutRoutes);
+
 //routes
 
-app.get('/', (req, res)=>{
-   res.json({mssg: "hello"});
-})
+// app.get('/', (req, res)=>{
+//    res.json({mssg: "hello"});
+// })
 
 //listen for requests
 app.listen(process.env.PORT, () =>
